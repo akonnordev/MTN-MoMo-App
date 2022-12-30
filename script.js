40184eec0b58b6e710db6d8134f7fedcd71c5e82
Akonnor Owusu Larbi <akonnor93@gmail.com>
2022-12-29 20:33:43 -0800

    commit for navbar script modification

--- script.js
+++ script.js
@@ -2,13 +2,18 @@
 const clickToOpen = document.querySelector(".doub");
 const mobileMenu = document.querySelector(".mobile-nav");
 const closeNav = document.querySelector(".close-nav");
+const mainNav2 = document.querySelector(".main-nav2");
 
 clickToOpen.addEventListener("click", function () {
   mobileMenu.classList.toggle("active");
+  mainNav2.classList.toggle("active2");
   document.querySelector(".main-nav h1").textContent = "Menu";
+  document.querySelector(".main-nav").style.left = "100%";
 });
 
 closeNav.addEventListener("click", function () {
   mobileMenu.classList.remove("active");
+  mainNav2.classList.remove("active2");
   document.querySelector(".main-nav h1").textContent = "Home";
+  document.querySelector(".main-nav").style.left = "0px";
 });
