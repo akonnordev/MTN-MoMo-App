62cce1998e62ae6b6910e31036b7b391c6d9b86a
Akonnor Owusu Larbi <akonnor93@gmail.com>
2022-12-29 21:29:35 -0800

    addition of footer

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ const clickToOpen = document.querySelector(".doub");
 const mobileMenu = document.querySelector(".mobile-nav");
 const closeNav = document.querySelector(".close-nav");
 const mainNav2 = document.querySelector(".main-nav2");
+const footerNav = document.querySelector(".footer-nav");
 
 clickToOpen.addEventListener("click", function () {
   mobileMenu.classList.toggle("active");
   mainNav2.classList.toggle("active2");
   document.querySelector(".main-nav h1").textContent = "Menu";
   document.querySelector(".main-nav").style.left = "100%";
+  footerNav.style.left = "100%";
 });
 
 closeNav.addEventListener("click", function () {
